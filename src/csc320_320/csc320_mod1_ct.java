ac076b7f94dd7be32b1735cc791bc4ded76bae29
william Atwooll <william.attwooll@csuglobal.com>
2026-08-22 12:59:07 -0600

    Adding module 1 Java code

---
+++ src/csc320_320/csc320_mod1_ct.java
@@ -0,0 +1,20 @@
+package csc320_320;
+
+public class csc320_mod1_ct {
+
+	public static void main(String[] args) {
+		// Declearing, assiging and printing requested information.
+		String first_name = "William";
+		String last_name = "Attwooll";
+		String street_address = "123 somewhare st.";
+		String city = "nowheresville";
+		String zip_code = "81212";
+		System.out.println("first name: " + first_name);
+		System.out.println("Last name: " + last_name);
+		System.out.println("Street: " + street_address);
+		System.out.println("City: " + city);
+		System.out.println("Zip code: " + zip_code);
+
+	}
+
+}
